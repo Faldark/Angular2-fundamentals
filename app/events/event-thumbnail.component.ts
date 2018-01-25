67c5c098e621e843fd0d529d83dc5ae5750bc1d7
Bogdan Chukhno <bogdan.chukhno@hys-enterprise.com>
2018-01-25 18:10:40 +0200

    finished on 7-th video.

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,18 +6,27 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     selector: 'event-thumbnail',
     template: `
     <div class="well howervell thumbnail">
-       <h2>{{event.name}}</h2>
-       <div>Date: {{event.date}}</div>
-       <div>Time: {{event.time}}</div>
-       <div>Price: \${{event.price}}</div>
+       <h2>{{event?.name}}</h2>
+       <div>Date: {{event?.date}}</div>
+       <div>Time: {{event?.time}}</div>
        <div>
-           <span>Location: {{event.location.address}}</span>
-           <span class="pad-left">{{event.location.city}}, {{event.location.country}}
+        <span>Early Start</span>
+        <span>Late Start</span>
+        <span>Normal Start</span>
+       </div>
+       <div>Price: \${{event?.price}}</div>
+       <div *ngIf="event?.location">
+           <span>Location: {{event?.location?.address}}</span>
+           <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}
            </span>
+       </div>
+       <div *ngIf="event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
        </div>
     </div>
     `,
     styles: [`
+        .thumbnail {min-height: 210px; }
         .pad-left { margin-left: 10px;}
         .well div {color: #bbb;}
     `]
